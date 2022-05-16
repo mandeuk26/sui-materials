0303035b6810e91eee0cd93a8f3bd7e012afc6fd
hyunjun_kang <hyunjun_kang@linecorp.com>
2022-05-16 17:58:51 +0900

    Update Chapter4 code

--- 04-building-for-testability/projects/starter/SwiftCalc/MemoryView.swift
+++ 04-building-for-testability/projects/starter/SwiftCalc/MemoryView.swift
@@ -29,25 +29,39 @@
 import SwiftUI
 
 struct MemoryView: View {
-  @Binding var memory: Double
-  var geometry: GeometryProxy
+    @Binding var memory: Double
+    var geometry: GeometryProxy
 
-  var body: some View {
-    HStack {
-      Spacer()
-      Text("\(memory)")
-        .padding(.horizontal, 5)
-        .frame(
-          width: geometry.size.width * 0.85,
-          alignment: .trailing
-        )
-        .overlay(
-          RoundedRectangle(cornerRadius: 8)
-            .stroke(lineWidth: 2)
-            .foregroundColor(Color.gray)
-        )
-      // Add gesture here
-      Text("M")
-    }.padding(.bottom).padding(.horizontal, 5)
-  }
+    var body: some View {
+        #if targetEnvironment(macCatalyst)
+        let eraseGesture = TapGesture(count: 2)
+            .onEnded { _ in
+                memory = 0.0
+            }
+
+        #else
+        let eraseGesture = DragGesture(minimumDistance: 20)
+            .onEnded { _ in
+                memory = 0.0
+            }
+        #endif
+
+        HStack {
+            Spacer()
+            Text("\(memory)")
+                .accessibilityIdentifier("memoryDisplay")
+                .padding(.horizontal, 5)
+                .frame(
+                    width: geometry.size.width * 0.85,
+                    alignment: .trailing
+                )
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(lineWidth: 2)
+                        .foregroundColor(Color.gray)
+                )
+                .gesture(eraseGesture)
+            Text("M")
+        }.padding(.bottom).padding(.horizontal, 5)
+    }
 }
